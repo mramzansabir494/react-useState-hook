75c5b71a3baf1997a940ae374b355fbbd3433db7
mramzansabir494 <mramzansabir494@gmail.com>
2022-09-08 13:48:54 +0500

    react useState hook first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App" style={{ color: color ? "red" : "green" }}>
+      <div className="container">
+        <input
+          placeholder="Type Inside ..."
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <br />
+        <br />
+        <p>Count: {count}</p>
+        <section>
+          <button onClick={() => setCount((previous) => previous - 1)}>
+            -
+          </button>
+          <button onClick={() => setCount((previous) => previous - 1)}>
+            +
+          </button>
+          <button onClick={() => setColor((previous) => !previous)}>
+            Color
+          </button>
+        </section>
+        <br />
+        <br />
+        <p>{userInput}</p>
+      </div>
+    </main>
   );
 }
 
